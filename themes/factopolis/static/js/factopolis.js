72218ec7abecc2eb7c3132674e266a3ea81731ea
Jeff Washington <jeff@factopolis.com>
2017-09-20 20:23:36 -0700

    Don't special-case YouTube; it's just like the other video types.

--- themes/factopolis/static/js/factopolis.js
+++ themes/factopolis/static/js/factopolis.js
@@ -12,40 +12,6 @@ $(document).ready(function () {
   }
 });
 
-/* YouTube videos */
-$(document).ready(function () {
-  $('a[href^="https://www.youtube.com/watch"]').on('click', function (e) {
-    var queryVars = (function (url) {
-      var p = document.createElement('a');
-      p.href = url;
-      if (p.search == "") return {};
-      var r = {};
-      (p.search.substring(1).split("&")).forEach(function (param) {
-        var kv = param.split('=');
-        r[kv[0]] = kv[1];
-      });
-      return r;
-    })($(this).attr('href'));
-
-    if ('v' in queryVars) {
-      e.preventDefault();
-
-      var videoUrl = 'https://www.youtube.com/embed/' + queryVars['v'] + '?autoplay=1&';
-      if ($(this).attr('data-start-time')) {
-        videoUrl += '&start=' + $(this).attr('data-start-time');
-        if ($(this).attr('data-end-time')) {
-          /* This actually ends the video; we want to pause it so
-             people can resume it if they want some more context… */
-          // videoUrl += '&end=' + $(this).attr('data-end-time');
-        }
-      }
-
-      $('#modal-target').html('<div class="embed-responsive embed-responsive-16by9"><iframe src="' + videoUrl + '" allowscriptaccess="always"></iframe></div>');
-      $('#mediaModal').modal();
-    }
-  });
-});
-
 /* HTML5 videos */
 $(document).ready(function () {
   $('.video-link').on('click', function (e) {
